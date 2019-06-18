75f7aef84b3eed74089853cad7cd589431cfe099
Patrick Stahl <p.stahl@shopware.com>
2019-06-18 08:14:33 +0200

    Initial commit

---
+++ src/ReadingPluginConfig.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Swag\ReadingPluginConfig;
+
+use Shopware\Core\Framework\Plugin;
+
+class ReadingPluginConfig extends Plugin
+{
+
+}
---
+++ src/Subscriber/MySubscriber.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace Swag\ReadingPluginConfig\Subscriber;
+
+use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityLoadedEvent;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Shopware\Core\Content\Product\ProductEvents;
+
+class MySubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var SystemConfigService
+     */
+    private $systemConfigService;
+
+    public function __construct(SystemConfigService $systemConfigService)
+    {
+        $this->systemConfigService = $systemConfigService;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ProductEvents::PRODUCT_LOADED_EVENT => 'onProductsLoaded'
+        ];
+    }
+
+    public function onProductsLoaded(EntityLoadedEvent $event): void
+    {
+        $exampleConfig = $this->systemConfigService->get('ReadingPluginConfig.config.example');
+    }
+}
